2b00ca430381da79564ecb5103fa3f7813f6f963
unknown <1559468033@qq.com>
2023-02-16 10:24:49 +0800

    封装axios请求

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<!--<template>-->
+
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "Home"-->
+<!--}-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
+<template>
+  <div>
+    Home
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<!--<template>-->
+
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "Login"-->
+<!--}-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
+<template>
+  <div>
+    Login
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
