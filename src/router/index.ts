7cf75e2bf974f5f9e2c59f06a89288d67c6660c3
unknown <1559468033@qq.com>
2023-02-17 15:52:03 +0800

    侧边栏基本结构开发

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import Home from '@/views/Home.vue'
 import Login from '@/views/login/index.vue'
 import {login} from "@/api/auth"
 import Dashboard from '@/views/dashboard/Dashboard.vue'
-
+import dashboard from "@/router/modules/dashboard";
 
 // 2. 定义一些路由
 // 每个路由都需要映射到一个组件。
@@ -15,13 +15,15 @@ const routes = [
     {path: '/dashboard', name: 'dashboard', component: Dashboard},
 ]
 
+const baseRoutes = [...routes, ...dashboard];
+
 // 3. 创建路由实例并传递 `routes` 配置
 // 你可以在这里输入更多的配置，但我们在这里
 // 暂时保持简单
 const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
     history: createWebHistory(),
-    routes, // `routes: routes` 的缩写
+    routes: baseRoutes, // `routes: routes` 的缩写
 })
 //配置前置守卫
 router.beforeEach((to, from, next) => {
